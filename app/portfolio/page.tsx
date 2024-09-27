77cab577ae9fe4c0e93039d36a2e358a3e955c21
Abel Getiso <abelgetiso@gmail.com>
2024-09-27 17:18:27 +0300

    feat: minor modifications;

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -19,8 +19,9 @@ export default function Projects() {
           logo: "TWC",
         },
       ],
-      description: "Demo description",
-      github_repo_link: "",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione incidunt eligendi, nulla provident alias nesciunt amet qui maxime quaerat ullam minima quasi, officiis dolore assumenda cum culpa eveniet? Excepturi voluptates sequi laudantium nisi eveniet cum modi voluptatum, autem vero veritatis libero omnis inventore nam possimus pariatur, magni eius corrupti quae!",
+      github_repo_link: "asdf",
     },
     {
       title: "Home Management",
@@ -39,7 +40,29 @@ export default function Projects() {
           logo: "TWC",
         },
       ],
-      description: "Demo description",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione incidunt eligendi, nulla provident alias nesciunt amet qui maxime quaerat ullam minima quasi, officiis dolore assumenda cum culpa eveniet? Excepturi voluptates sequi laudantium nisi eveniet cum modi voluptatum, autem vero veritatis libero omnis inventore nam possimus pariatur, magni eius corrupti quae!",
+      github_repo_link: "",
+    },
+    {
+      title: "AAU Blog Post",
+      banner: "Banner",
+      technologies: [
+        {
+          name: "Next.js",
+          logo: "NXTJ",
+        },
+        {
+          name: "daisyui",
+          logo: "DU",
+        },
+        {
+          name: "tailwindcss",
+          logo: "TWC",
+        },
+      ],
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione incidunt eligendi, nulla provident alias nesciunt amet qui maxime quaerat ullam minima quasi, officiis dolore assumenda cum culpa eveniet? Excepturi voluptates sequi laudantium nisi eveniet cum modi voluptatum, autem vero veritatis libero omnis inventore nam possimus pariatur, magni eius corrupti quae!",
       github_repo_link: "",
     },
   ];
--- app/portfolio/ui/ProjectCard.tsx
+++ app/portfolio/ui/ProjectCard.tsx
@@ -19,30 +19,35 @@ export default function ProjectCard({ project }: { project: ProjectCardProp }) {
     project;
 
   return (
-    <div className="neo-morph-dark rounded-md w-3/5 h-fit p-5 my-5 mx-auto">
+    <div className="neo-morph-dark rounded-r-3xl w-4/5 h-fit p-5 my-5 mx-auto">
       <div className="flex w-full">
-        <div className="w-4/5">
-          <div>{title}</div>
-          {/* BANNER GOES HERE */}
-          <div></div>
-          <div className="flex">
+        <div className="w-2/5 flex items-center justify-center">
+          <p>BANNER</p>
+        </div>
+        <div className="w-3/5">
+          <div className="mb-5 font-semibold">{title}</div>
+          <div className="flex w-3/5">
             {technologies?.map((tech, idx) => (
-              <div key={idx} className="mr-5 my-5">
+              <div key={idx} className="mr-5 mb-5">
                 <Tooltip key={tech?.name} content={tech?.name ?? ""}>
                   <button>{tech.logo}</button>
                 </Tooltip>
               </div>
             ))}
           </div>
-          <div>{description}</div>
-        </div>
-        {github_repo_link != "" && (
-          <div className="w-1/5 flex items-center justify-center">
-            <Link href={github_repo_link}>
-              <p>GitHub Link</p>
-            </Link>
+          <div className="mb-5">
+            {description.length > 250
+              ? description.substring(0, 250) + "..."
+              : description}
           </div>
-        )}
+          <div>
+            {github_repo_link != "" && (
+              <Link href={github_repo_link}>
+                <p>GitHub Link</p>
+              </Link>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
